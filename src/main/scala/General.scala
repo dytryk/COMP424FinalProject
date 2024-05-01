40b69d0ce0a1931d668c93aa62cf5b5910892215
dytryk <astonmartinone111111@gmail.com>
2024-05-01 19:31:16 -0400

    start

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -1,3 +1,33 @@
+import scala.util.Random
 
 @main
-def general() = { println("Hello World!" )}
+def general() = {
+  val ten = new Array[Int](10)
+  val thousand = new Array[Int](1000)
+  val million = new Array[Int](1000000)
+  val billion = new Array[Int](1000000000)
+  val rand = new Random()
+  
+  for (i <- 0 until 10) {
+    ten(i) = rand.nextInt(101)
+  }
+  for (i <- 0 until 1000) {
+    thousand(i) = rand.nextInt(101) 
+  }
+  for (i <- 0 until 1000000) {
+    million(i) = rand.nextInt(101)
+  }
+  for (i <- 0 until 1000000000) {
+    billion(i) = rand.nextInt(101)
+  }
+  
+  merge(ten)
+  merge(thousand)
+  merge(million)
+  merge(billion)
+
+}
+
+def merge(A: Array[Int]): List[Int] = {
+
+}
