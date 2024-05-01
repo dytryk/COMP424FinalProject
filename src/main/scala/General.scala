e2ff06783460d715006410afa4138a0790af4780
dytryk <astonmartinone111111@gmail.com>
2024-05-01 19:38:42 -0400

    start

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -5,41 +5,33 @@ def general() = {
   val ten = new Array[Int](10)
   val thousand = new Array[Int](1000)
   val million = new Array[Int](1000000)
-  val billion = new Array[Int](1000000000)
   val rand = new Random()
-  
+
   for (i <- 0 until 10) {
     ten(i) = rand.nextInt(101)
   }
   for (i <- 0 until 1000) {
-    thousand(i) = rand.nextInt(101) 
+    thousand(i) = rand.nextInt(101)
   }
   for (i <- 0 until 1000000) {
     million(i) = rand.nextInt(101)
   }
-  for (i <- 0 until 1000000000) {
-    billion(i) = rand.nextInt(101)
-  }
-  
+
   mergeSort(ten)
   mergeSort(thousand)
   mergeSort(million)
-  mergeSort(billion)
-  
+
   quickSort(ten)
   quickSort(thousand)
   quickSort(million)
-  quickSort(billion)
-  
+
   radixSort(ten)
   radixSort(thousand)
   radixSort(million)
-  radixSort(billion)
 
   heapSort(ten)
   heapSort(thousand)
   heapSort(million)
-  heapSort(billion)
 }
 
 def mergeSort(A: Array[Int]): Array[Int] = {
