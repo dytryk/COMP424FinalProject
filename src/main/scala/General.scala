9e74d8f9bcc2fdee470dc3d2db9f3ed51936f7fa
dytryk <astonmartinone111111@gmail.com>
2024-05-01 21:01:39 -0400

    betterradix

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -1,22 +1,26 @@
 import scala.util.Random
+import scala.collection.parallel.CollectionConverters.*
 
 @main
 def general() = {
   val ten = new Array[Int](10)
   val thousand = new Array[Int](1000)
   val million = new Array[Int](1000000)
+  val hundredMillion = new Array[Int](100000000)
   val rand = new Random()
 
   for (i <- 0 until 10) {
-    ten(i) = rand.nextInt(101)
+    ten(i) = rand.nextInt(1000)
   }
   for (i <- 0 until 1000) {
-    thousand(i) = rand.nextInt(101)
+    thousand(i) = rand.nextInt(1000)
   }
   for (i <- 0 until 1000000) {
-    million(i) = rand.nextInt(101)
+    million(i) = rand.nextInt(1000)
   }
-
+  for (i <- 0 until 100000000) {
+    hundredMillion(i) = rand.nextInt(1000)
+  }
 
   val (timeMergeSort10, _) = timeIt(mergeSort(ten))
   println(s"Merge Sort (10 elements): ${timeMergeSort10} ms")
@@ -39,11 +43,38 @@ def general() = {
   val (timeRadixSort10, _) = timeIt(radixSort(ten))
   println(s"Radix Sort (10 elements): ${timeRadixSort10} ms")
 
+  print(ten.mkString("Array(", ", ", ")") + "\n")
+  print(radixSort(ten).mkString("Array(", ", ", ")") + "\n")
+
   val (timeRadixSort1000, _) = timeIt(radixSort(thousand))
   println(s"Radix Sort (1000 elements): ${timeRadixSort1000} ms")
 
+//  print(thousand.mkString("Array(", ", ", ")") + "\n")
+//  print(radixSort(thousand).mkString("Array(", ", ", ")") + "\n")
+
   val (timeRadixSort1000000, _) = timeIt(radixSort(million))
   println(s"Radix Sort (1000000 elements): ${timeRadixSort1000000} ms")
+
+  val (timeRadixSort100000000, _) = timeIt(radixSort(hundredMillion))
+  println(s"Radix Sort (100000000 elements): ${timeRadixSort100000000} ms")
+
+  val (timeRadixSortPar10, _) = timeIt(radixSortPar(ten))
+  println(s"Parallel Radix Sort (10 elements): ${timeRadixSortPar10} ms")
+
+  print(ten.mkString("Parallel Array(", ", ", ")") + "\n")
+  print(radixSort(ten).mkString("Parallel Array(", ", ", ")") + "\n")
+
+  val (timeRadixSortPar1000, _) = timeIt(radixSortPar(thousand))
+  println(s"Parallel Radix Sort (1000 elements): ${timeRadixSortPar1000} ms")
+
+  //  print(thousand.mkString("Parallel Array(", ", ", ")") + "\n")
+  //  print(radixSort(thousand).mkString("Parallel Array(", ", ", ")") + "\n")
+
+  val (timeRadixSortPar1000000, _) = timeIt(radixSortPar(million))
+  println(s"Parallel Radix Sort (1000000 elements): ${timeRadixSortPar1000000} ms")
+
+  val (timeRadixSortPar100000000, _) = timeIt(radixSortPar(hundredMillion))
+  println(s"Parallel Radix Sort (100000000 elements): ${timeRadixSortPar100000000} ms")
 
   val (timeHeapSort10, _) = timeIt(heapSort(ten))
   println(s"Heap Sort (10 elements): ${timeHeapSort10} ms")
@@ -96,6 +127,53 @@ def countingSort(A: Array[Int], exp: Int): Array[Int] = {
   for (i <- A.indices.reverse) {
     B(C((A(i) / exp) % 10) - 1) = A(i)
     C((A(i) / exp) % 10) -= 1
+  }
+
+  B
+}
+
+def radixSortPar(A: Array[Int]): Array[Int] = {
+  val max = A.max
+  var exp = 1
+  var B = A
+
+  while (max / exp > 0) {
+    B = countingSortPar(B, exp)
+    exp *= 10
+  }
+
+  B
+}
+
+def countingSortPar(A: Array[Int], exp: Int): Array[Int] = {
+  val max: Int = A.max
+  val C = Array.fill(max + 1)(0)
+  val B = Array.fill(A.length)(0)
+
+//  for (num <- A) {C((num / exp) % 10) += 1}
+
+//  A.par.foreach(num => {
+//    C((num / exp) % 10) += 1
+//  })
+
+  A.par.foreach(num => {
+    synchronized {
+      C((num / exp) % 10) += 1
+    }
+  })
+
+  for (i <- 1 until C.length) {C(i) += C(i - 1)}
+
+//  for (i <- A.indices.reverse) {
+//    B(C((A(i) / exp) % 10) - 1) = A(i)
+//    C((A(i) / exp) % 10) -= 1
+//  }
+
+  A.indices.reverse.foreach { i =>
+    synchronized {
+      B(C((A(i) / exp) % 10) - 1) = A(i)
+      C((A(i) / exp) % 10) -= 1
+    }
   }
 
   B
